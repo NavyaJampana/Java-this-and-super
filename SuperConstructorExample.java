f091eccf8e80bed652c1bb08b30d71e0367eefca
NavyaJampana <jnavya2024@gmail.com>
2025-03-08 17:24:51 +0530

    Add files via upload

---
+++ SuperConstructorExample.java
@@ -0,0 +1,19 @@
+package Thisandsuper;
+
+class ParentClass {
+    ParentClass() {
+        System.out.println("Parent Constructor");
+    }
+}
+
+class SuperConstructorExample extends ParentClass {
+    SuperConstructorExample() {
+        super();  // Calls Parent Constructor
+        System.out.println("Child Constructor");
+    }
+
+    public static void main(String[] args) {
+        new SuperConstructorExample();
+    }
+}
+
